2ff475634d1c925d449ef61e3ee3aaf902848a17
Pablo Valente <pablo.nvalente@gmail.com>
2023-01-19 18:01:44 -0300

    fix add types

--- src/components/utils/GameCard.tsx
+++ src/components/utils/GameCard.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-const GameCard = ({ pictureUrl, title, category }) => {
+interface Props {
+  pictureUrl: string;
+  title: string;
+  category: string;
+}
+
+const GameCard = ({ pictureUrl, title, category }: Props) => {
   const formattedTitle = title.toLowerCase().split(' ').join('-');
   return (
     <Link href={`/${category}/${formattedTitle}`}>
-      <article className='border-zinc-900 border-2 rounded-lg overflow-hidden'>
+      <article className='overflow-hidden border-2 rounded-lg border-zinc-900'>
         <Image src={pictureUrl} width={500} height={500} alt={title} className='' />
         <div className='p-2'>
           <h2 className='text-sm'>{title}</h2>
--- src/components/utils/SectionTitle.tsx
+++ src/components/utils/SectionTitle.tsx
@@ -1,4 +1,8 @@
-const SectionTitle = ({ text }) => {
+interface Props {
+  text: string;
+}
+
+const SectionTitle = ({ text }: Props) => {
   return (
     <div className='mb-4 bg-[#111] p-4 rounded-lg'>
       <h1 className='text-sm font-bold uppercase text-zinc-400'>{text}</h1>
