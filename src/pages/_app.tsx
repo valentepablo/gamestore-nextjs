a2b2e84aed68eddce98656de5a194cd4953d735e
Pablo Valente <pablo.nvalente@gmail.com>
2023-01-20 00:33:50 -0300

    testing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,20 @@ import '../styles/globals.css';
 import Layout from '../components/layout/Layout';
 import { CartContextProvider } from '../components/context/CartContext';
 import { AppProps } from 'next/app';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <CartContextProvider>
-        <Component {...pageProps} />
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 1 } }}
+            exit={{ opacity: 0, transition: { duration: 1 } }}>
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
       </CartContextProvider>
     </Layout>
   );
