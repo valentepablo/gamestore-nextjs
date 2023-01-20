2a24f42d27dbf965c2d2fd368f55141d53ebb3c1
Pablo Valente <pablo.nvalente@gmail.com>
2023-01-20 00:58:29 -0300

    add random animations

--- src/components/utils/SectionTitle.tsx
+++ src/components/utils/SectionTitle.tsx
@@ -1,12 +1,19 @@
+import { motion } from 'framer-motion';
+
 interface Props {
   text: string;
 }
 
 const SectionTitle = ({ text }: Props) => {
   return (
-    <div className='mb-4 bg-[#111] p-4 rounded-lg'>
-      <h1 className='text-sm font-bold uppercase text-zinc-400'>{text}</h1>
-    </div>
+    <>
+      <motion.div
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        className='mb-4 bg-[#111] p-4 rounded-lg'>
+        <h1 className='text-sm font-bold uppercase text-zinc-400'>{text}</h1>
+      </motion.div>
+    </>
   );
 };
 
