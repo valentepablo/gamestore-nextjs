c6fad53167d0f432b42eda2d4b907145e5f206d4
Pablo Valente <pablo.nvalente@gmail.com>
2023-01-20 00:52:39 -0300

    add page transitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,32 @@ import Layout from '../components/layout/Layout';
 import { CartContextProvider } from '../components/context/CartContext';
 import { AppProps } from 'next/app';
 import { motion, AnimatePresence } from 'framer-motion';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
     <Layout>
       <CartContextProvider>
-        <AnimatePresence>
+        <AnimatePresence mode='wait'>
           <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1, transition: { duration: 1 } }}
-            exit={{ opacity: 0, transition: { duration: 1 } }}>
+            key={router.asPath}
+            initial='initialState'
+            animate='animateState'
+            exit='exitState'
+            variants={{
+              initialState: {},
+              animateState: {
+                x: 0,
+                opacity: 1,
+                transition: { duration: 0.2 },
+              },
+              exitState: {
+                x: '120%',
+                opacity: 0,
+                transition: { duration: 0.2 },
+              },
+            }}>
             <Component {...pageProps} />
           </motion.div>
         </AnimatePresence>
